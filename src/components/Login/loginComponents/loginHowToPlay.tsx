f2cbfc9f861f6649d1547d75c954e7df4e77259d
Adrian Lopez <lopez120696@gmail.com>
2021-07-16 23:30:54 -0500

    merge changes

--- src/components/Login/loginComponents/loginHowToPlay.tsx
+++ src/components/Login/loginComponents/loginHowToPlay.tsx
@@ -11,7 +11,7 @@ const LoginHowToPlay: React.FC<any> = () => {
                     <div className="howtoplay">
                         <h1>How To Play</h1>
                         <div>
-                            <h3>Aqui va a ir un css del ahorcado xd</h3>
+                            <h3>Welcome to my cribs bitch</h3>
                         </div>
                         <div>
                                 <h3>Invite 3 Friends</h3>
