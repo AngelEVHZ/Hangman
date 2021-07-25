8293fc00f699989cb43af97808285034bc9d2cc2
Adrian Lopez <lopez120696@gmail.com>
2021-07-25 18:42:14 -0500

    merge

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,17 +4,21 @@ import Players from "./dashboardComponents/players";
 import { Button } from "react-bootstrap";
 
 
+import GameMode from "./dashboardComponents/gameMode";
 
 const Dashboard: React.FC<any> = () => {
     const {handle, state} = UseDashboardState();
     
     return (
         <div>
-            <Players players={state.players}></Players>     
             <div className="row">
                 <div className="col-md-6">
-                    <Button onClick={handle.initMatch}> PLAY</Button>
+                <Players players={state.players}></Players>
                 </div>
+                <div className="col-md-6">
+                    <GameMode></GameMode>     
+                </div>
+                <Button onClick={handle.initMatch}> PLAY</Button>
             </div>
         </div>
     );
--- src/components/Dashboard/dashboardComponents/gameMode.tsx
+++ src/components/Dashboard/dashboardComponents/gameMode.tsx
@@ -1,1 +1,36 @@
 import React from "react";
+import './gameStyle.css';
+import {Card} from "react-bootstrap";
+
+const GameMode: React.FC<any> = () => {
+
+    return (
+        <div>
+           <div className="container">
+                <div className="row">
+                    <div className="col-md-3">
+                    <Card className="gameCard">
+                        <p>Normal</p>
+                    </Card>
+                    </div>
+                    <div className="col-md-3">
+                    <Card className="gameCard">
+                        <p>Contra Reloj</p>
+                    </Card>
+                    </div>
+                    <div className="col-md-3">
+                    <Card className="gameCard">
+                        <p>Proximamente</p>
+                    </Card>
+                    </div>
+                    <div className="col-md-3">
+                    <Card className="gameCard">
+                        <p>Proximamente</p>
+                    </Card>
+                    </div>
+                </div>
+           </div>
+        </div>
+    );
+};
+export default GameMode;
