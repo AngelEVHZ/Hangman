9b2e91f1c210ea458b7bea6f78d6a61b522088dc
AngelEVH <angel.valenzuela@kushkipagos.com>
2021-07-30 01:32:57 -0500

    add hover to cards

--- src/components/Dashboard/dashboardComponents/gameCard.tsx
+++ src/components/Dashboard/dashboardComponents/gameCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import './gameStyle.css';
-import {Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export interface GameCardProps {
     title: String;
@@ -9,16 +9,25 @@ export interface GameCardProps {
 }
 
 const GameCard: React.FC<any> = (props: GameCardProps) => {
-
+    const [onHover, setOnHover] = useState(false);
+    const colorClass = onHover ? "gameCardHover" : "";
     return (
-        <div>
-         <Card className="gameCard">
-             <p className="title-card">{props.title}</p> 
-                <div className="mask">
-                 <p className="description-card">{props.description}</p>
-                </div>
-             {props.icon}
-         </Card>          
+        <div className="">
+            <Card className={`gameCard ${colorClass}`}
+                onMouseEnter={() => setOnHover(true)}
+                onMouseLeave={() => setOnHover(false)}>
+                {!onHover &&
+                    <>
+                        <p className="title-card">{props.title}</p>
+                        {props.icon}
+                    </>
+                }
+                {onHover &&
+                    <div>
+                        <p className="description-card">{props.description}</p>
+                    </div>
+                }
+            </Card>
         </div>
     );
 };
