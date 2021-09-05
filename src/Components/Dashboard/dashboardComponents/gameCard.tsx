29bbf44574aa1c613d031bd6b12bf539aa1b2356
Mario Adrian Lopez Mendez <mlopez@redcompanies.com.mx>
2021-09-05 01:01:04 -0500

    Se agrego tags de idioma de modo de juego

--- src/Components/Dashboard/dashboardComponents/gameCard.tsx
+++ src/Components/Dashboard/dashboardComponents/gameCard.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { useLanguage } from "../../../Context/LanguageProvider";
 import './gameStyle.css';
+import { get } from "lodash";
+
 
 export interface GameCardProps {
     id: string;
-    title: String;
+    title: string;
     description: string;
     icon: JSX.Element;
     available: boolean;
@@ -17,6 +20,7 @@ interface GameCardLocalProps {
 }
 
 const GameCard: React.FC<any> = (props: GameCardLocalProps) => {
+    const { lang } = useLanguage();
     const { item , selected} = props;
     const [onHover, setOnHover] = useState(false);
     const colorClass = onHover ? "game-card-hover" : selected ? "game-card-selected" : "";
@@ -29,20 +33,20 @@ const GameCard: React.FC<any> = (props: GameCardLocalProps) => {
                     onClick={() => props.selectGame(item)}>
                     {!onHover &&
                         <>
-                            <p className="title-card">{item.title}</p>
+                            <p className="title-card">{get(lang, item.title)}</p>
                             {item.icon}
                         </>
                     }
                     {onHover &&
                         <div>
-                            <p className="description-card">{item.description}</p>
+                            <p className="description-card">{get(lang, item.description)}</p>
                         </div>
                     }
                 </div>
             }
             {!item.available &&
                 <div className="card gameCard not-available">
-                    <p className="title-card">{item.title}</p>
+                    <p className="title-card">{get(lang, item.title)}</p>
                     {item.icon}
                 </div>
 
